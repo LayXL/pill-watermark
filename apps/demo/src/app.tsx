79f088da3a3f7cd5457ef52d7bb88744b3e58ca9
Виталий <46228376+LayXL@users.noreply.github.com>
2024-11-28 03:24:45 +0500

    Improvements in demo-app

--- apps/demo/src/app.tsx
+++ apps/demo/src/app.tsx
@@ -1,8 +1,6 @@
 import { PillWatermark } from "pill-watermark"
 import "pill-watermark/dist/index.css"
 
-function App() {
+export default function App() {
   return <PillWatermark alwaysVisible={true}>Yep!</PillWatermark>
 }
-
-export default App
--- apps/demo/vite.config.ts
+++ apps/demo/vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+import { VitePWA } from "vite-plugin-pwa"
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: "Pill Watermark",
+        short_name: "PW",
+        description: "Pill Watermark App",
+        theme_color: "#ffffff",
+        display: "standalone",
+        start_url: "/",
+      },
+    }),
+  ],
 })
